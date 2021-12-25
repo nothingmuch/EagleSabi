3c4442cc4fd3c39f77a00b61eff3d39d2eccdfa3
Yuval Kogman <nothingmuch@woobling.org>
2021-12-25 06:04:56 +0200

    Introduce SpendableCoin & SpendableSmartCoin
    Two separate records are provided with implicit conversion, but this is intended to be refactored into well defined interfaces.

--- WalletWasabi/WabiSabi/Client/CoinJoinClient.cs
+++ WalletWasabi/WabiSabi/Client/CoinJoinClient.cs
@@ -103,7 +103,7 @@ namespace WalletWasabi.WabiSabi.Client
 				InCriticalCoinJoinState = true;
 
 				// Calculate outputs values
-				var registeredCoins = registeredAliceClients.Select(x => x.SmartCoin.Coin);
+				var registeredCoins = registeredAliceClients.Select(x => x.SpendableCoin.SmartCoin.Coin);
 				var availableVsize = registeredAliceClients.SelectMany(x => x.IssuedVsizeCredentials).Sum(x => x.Value);
 
 				// Calculate outputs values
@@ -203,7 +203,7 @@ namespace WalletWasabi.WabiSabi.Client
 			}
 
 			// Gets the list of scheduled dates/time in the remaining available time frame when each alice has to be registered.
-			var remainingTimeForRegistration = ( roundState.InputRegistrationEnd - DateTimeOffset.UtcNow ) - TimeSpan.FromSeconds(15);
+			var remainingTimeForRegistration = (roundState.InputRegistrationEnd - DateTimeOffset.UtcNow) - TimeSpan.FromSeconds(15);
 
 			Logger.LogDebug($"Round ({roundState.Id}): Input registration started, it will end in {remainingTimeForRegistration.TotalMinutes} minutes.");
 
---
+++ WalletWasabi/WabiSabi/Client/SpendableCoin.cs
@@ -0,0 +1,20 @@
+using NBitcoin;
+
+namespace WalletWasabi.WabiSabi.Client
+{
+	public record SpendableCoin(Coin Coin, BitcoinSecret BitcoinSecret)
+	{
+		// TODO provide minimal interfaces
+		public Money Amount => Coin.Amount;
+		public Script ScriptPubKey => Coin.ScriptPubKey;
+		public OutPoint OutPoint => Coin.Outpoint;
+
+		[Obsolete("not sure which one to deprecate to be honest, but OutPoint vs Outpoint is inconsistent. follow Core?")]
+		public OutPoint Outpoint => Coin.Outpoint;
+
+		public void Sign(Transaction transaction)
+		{
+			transaction.Sign(BitcoinSecret, Coin);
+		}
+	}
+}
